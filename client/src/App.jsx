428b8f266d27fc69f512cee0ed14acd8562dd7a7
Andrew Gavgavian <agavgavi@ucsc.edu>
2021-11-27 16:29:30 -0800

    basic app for starting

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Code goes here.
+      </header>
+    </div>
+  );
+}
+
+export default App;

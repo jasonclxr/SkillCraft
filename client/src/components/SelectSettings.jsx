a25fa98a31ea28824f3f8a9fe73b797d554381c1
Andrew Gavgavian <andrewgee7@gmail.com>
2021-11-29 22:54:45 -0800

    map loop, remove component.

--- client/src/components/SelectSettings.jsx
+++ client/src/components/SelectSettings.jsx
@@ -1,11 +1,26 @@
 import React, { useContext, useState } from 'react';
 import { Form, Button, Col, Row } from 'react-bootstrap';
-import SkillSlider from './SkillSlider';
+
+const sumValues = (obj) => {
+    let total = 0;
+    for (const x in obj) {
+        total += parseInt(obj[x]);
+    }
+    return total;
+}
 
 const SelectSettings = () => {
+    const [value, setValue] = useState({ agility: 0, stealth: 0, survivability: 0, magic: 0, signs: 0 });
+    const MAX_TOTAL = 100;
+    let left = MAX_TOTAL - sumValues(value);
     const handleSubmit = async (e) => {
         e.preventDefault();
     };
+
+    const calculateSliders = (e, key) => {
+        setValue({ ...value, [key]: e.target.value })
+    };
+
     return (
         <Form>
             <Form.Group as={Row} className="mb-3">
@@ -13,24 +28,23 @@ const SelectSettings = () => {
                     Available Points
                 </Form.Label>
                 <Col sm="auto">
-                    <Form.Control type="number"/>
+                    <Form.Control type="number" />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-                <SkillSlider label="Agility"/>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-                <SkillSlider label="Stealth"/>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-                <SkillSlider label="Survivability"/>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-                <SkillSlider label="Magic"/>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-                <SkillSlider label="Signs"/>
-            </Form.Group>
+            {Object.keys(value).map((key, index) => {
+                return (
+                    <Form.Group as={Row} className="mb-3" key={index}>
+                        <Form.Label column sm={1}>
+                            {key}
+                        </Form.Label>
+                        <Col sm="auto">
+                            <Form.Range value={value[key]} onChange={e => calculateSliders(e, key)} min={0} max={100} />
+                        </Col>
+                        <Col>
+                            {value[key]} / {left}
+                        </Col>
+                    </Form.Group>);
+            })}
 
             <Form.Group as={Row} className="mb-3">
                 <Col sm={{ span: 10, offset: 1 }}>

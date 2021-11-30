6900ff7bc0df15fdefaeb494d2455fd3a062e2a0
Ivan Martinez-Arias <50786762+ivalmart@users.noreply.github.com>
2021-11-29 20:23:28 -0800

    Adding half of skill tree + js file

---
+++ mcts.js
@@ -0,0 +1,34 @@
+// Note: might want to consider max win rates of children nodes
+
+num_nodes = 1000
+explore_factor = 2
+
+function main() {
+    
+}
+
+// Traverse graph using UCT function until leaf node is reached
+function traverse_nodes() {
+
+}
+
+// Adds a new leaf to the tree by creating a new child node for the given node.
+// Constraints: Must have enough skill points in tree to reach row
+function expand_leaf() {
+
+}
+
+// Selects random skills until points are depleted
+function rollout() {
+
+}
+
+// Propagate result back through the graph
+function backpropagate() {
+    
+}
+
+// Performs MCTS by sampling games and returns the action
+function think() {
+
+}

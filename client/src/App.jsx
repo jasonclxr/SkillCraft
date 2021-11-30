9bf5f8a4e83cc1433ffcc9c5429815b4bc41e1f3
Andrew Gavgavian <andrewgee7@gmail.com>
2021-11-29 19:57:33 -0800

    reverted back to bootstrap with library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import { AppContextProvider } from './context/AppContext';
-import SliderSizes from './components/Slider';
+
+import { Container } from 'react-bootstrap';
+
 function App() {
   return (
     <AppContextProvider>
-      <div className="container">
+      <Container>
         <Router>
             <Routes>
                 <Route exact path="/" element={<Home />} />
             </Routes>
         </Router>
-      </div>
-      <SliderSizes />
+        </Container>
     </AppContextProvider>
   );
 }
--- client/src/components/SkillSlider.jsx
+++ client/src/components/SkillSlider.jsx
@@ -1,16 +1,20 @@
 import React, { useContext, useState } from 'react';
 
+import { Form, Col } from 'react-bootstrap';
+
 const SkillSlider = (props) => {
     const [value, setValue] = useState(0);
     return (
         <>
-            <label for="customRange1" className="col-sm-1 col-form-label">{props.label}</label>
-            <div className="col-auto">
-                <input id="customRange1" value={value} onChange={e => setValue(e.target.value)} type="range" className="form-range" min={0} max={100} />
-            </div>
-            <div className="col-auto">
+            <Form.Label column sm={1}>
+                {props.label}
+            </Form.Label>
+            <Col sm="auto">
+                <Form.Range value={value} onChange={e => setValue(e.target.value)}  min={0} max={100}/>
+            </Col>
+            <Col>
                 {value}
-            </div>
+            </Col>
         </>
     )
 };

41378b28e076cd42f47d39c6af47a8d57b678d0a
Jason Blecman <jblecman@ucsc.edu>
2021-12-05 21:52:07 -0800

    Fancyyy

--- client/src/components/Styles.js
+++ client/src/components/Styles.js
@@ -12,5 +12,21 @@ export const background = {
   top: '-5vh',
   zIndex: -1000,
   filter: 'blur(5px) invert(25%)',
-
 };
+
+export const gridItem = {
+  backgroundColor: '#ced0d4',
+  padding: '15px 0px 15px 0px',
+  borderRadius: 10,
+  margin: '0 10px 10px 0',
+  textAlign: 'center',
+  verticalAlign: 'middle',
+}
+
+export const rowItem = {
+  margin: '0 0 0 0',
+}
+
+export const container = {
+  padding: '0 0 0 0',
+}
